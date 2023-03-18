23f58047cc5b5e2588731cea238a41c42fb6cbcb
Sam Allen <samallen659@gmail.com>
2023-03-18 15:23:54 +0000

    updated search with validation styling

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -9,6 +9,7 @@ type AppProps = {
 
 function SearchBar({ search }: AppProps) {
     const [searchValue, setSearchValue] = useState('');
+    const [formError, setFormError] = useState(false);
 
     const handleChange = (e: any) => {
         setSearchValue(e.target.value)
@@ -16,6 +17,11 @@ function SearchBar({ search }: AppProps) {
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
+        if(searchValue === ""){
+            setFormError(true);
+            return;
+        }
+        setFormError(false);
         search(searchValue);
     }
     return (
@@ -24,9 +30,11 @@ function SearchBar({ search }: AppProps) {
             <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 </div>
-                <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 dark:text-white border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-900 dark:bg-gray-600" placeholder="Search for any word..." onChange={handleChange} required />
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"><img src={searchIcon} /></button>
+                <input type="search" id="default-search" className={`block w-full p-4 pl-10 lg:text-lg text-gray-900 dark:text-white border ${formError ? "border-red" : "border-gray-300 dark:border-gray-900"} rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600`} placeholder="Search for any word..." onChange={handleChange} formNoValidate />
+                <button type="submit" className="text-white absolute top-1/2 right-2.5 transform -translate-x-1/2 -translate-y-1/2
+ focus:outline-none font-medium rounded-lg text-sm px-4 py-2"><img src={searchIcon} /></button>
             </div>
+            <p className={`${formError ? "visible" : "invisible"} text-red lg:text-lg`}>Whoops, can't be empty...</p>
         </form>
     )
 }
