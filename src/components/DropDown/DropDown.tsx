fa496a9ff79d5c941b6f11b3ca6a0c15cd2d0fb2
Sam Allen <samallen659@gmail.com>
2023-03-18 15:11:17 +0000

    added shadow to font selection drop down

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -18,10 +18,10 @@ function DropDown({ handleFontChange }: AppProps) {
         <div>
             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:text-white" type="button">{fontDisplayOptions[selected]}</button>
             {/* Dropdown menu */}
-            <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 shadow-gray-200 shadow-lg dark:shadow-purple">
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                     {fontOptions.map((option, i) => (
-                        <li key={i} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={handleClick} value={i}>
+                        <li key={i} className={`font-${fontOptions[i]} block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ${selected === i && 'text-purple font-bold'}`} onClick={handleClick} value={i}>
                             {option}
                         </li>
                     ))}
